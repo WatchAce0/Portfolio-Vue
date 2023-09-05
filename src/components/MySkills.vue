e24f1a09f310fb0861b5d7d0c5690de0da00b7f7
WatchAce0 <hereforyesterday@gmail.com>
2023-09-05 18:28:07 -0400

    Updated copy for background and skills.

--- src/components/MySkills.vue
+++ src/components/MySkills.vue
@@ -2,14 +2,17 @@
   <section id="skills" class="p-8">
     <div class="container mx-auto text-center">
       <h2 class="text-4xl font-bold mb-8">Skills</h2>
-      <ul class="flex flex-wrap justify-center gap-4">
+      <ul class="flex flex-wrap justify-center p-6 gap-4">
         <li class="bg-blue-500 text-white rounded-full px-6 py-2">HTML</li>
         <li class="bg-blue-500 text-white rounded-full px-6 py-2">CSS</li>
-        <li class="bg-blue-500 text-white rounded-full px-6 py-2">
-          JavaScript
-        </li>
+        <li class="bg-blue-500 text-white rounded-full px-6 py-2">React.js</li>
+        <li class="bg-blue-500 text-white rounded-full px-6 py-2">Next.js</li>
         <li class="bg-blue-500 text-white rounded-full px-6 py-2">Vue.js</li>
         <li class="bg-blue-500 text-white rounded-full px-6 py-2">Node.js</li>
+        <li class="bg-blue-500 text-white rounded-full px-6 py-2">Firebase</li>
+        <li class="bg-blue-500 text-white rounded-full px-6 py-2">Stripe</li>
+        <li class="bg-blue-500 text-white rounded-full px-6 py-2">Vercel</li>
+        <li class="bg-blue-500 text-white rounded-full px-6 py-2">Netlify</li>
       </ul>
     </div>
   </section>
