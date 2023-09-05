7829215a1b394b9436c6c93273a10e08634cc35b
WatchAce0 <hereforyesterday@gmail.com>
2023-09-05 02:07:20 -0400

    Font Resizing

--- src/components/TheBackground.vue
+++ src/components/TheBackground.vue
@@ -2,7 +2,7 @@
   <section id="background" class="bg-gray-200">
     <div :class="{ 'bg-white dark:bg-gray-900': isDark }">
       <h2 class="text-4xl text-center font-bold mb-8">Image Goes Here</h2>
-      <p class="text-center">
+      <p class="text-center text-xl">
         Hey, I am Joe DiGioia a Full Stack Developer and Graphic Designer from
         Pittsburgh, PA.
       </p>
