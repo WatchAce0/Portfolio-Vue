a77b0d5db9fdfdcd33bbaeb01bb43d6f0a613d51
WatchAce0 <hereforyesterday@gmail.com>
2023-09-05 18:40:19 -0400

    Updated Copy for Bio.

--- src/components/TheBackground.vue
+++ src/components/TheBackground.vue
@@ -3,10 +3,10 @@
     <div :class="{ 'bg-white dark:bg-gray-900': isDark }">
       <h2 class="text-4xl text-center font-bold mb-8">Image Goes Here</h2>
       <p class="text-center text-xl p-8">
-        Hey, I'm' Joe DiGioia a Full Stack Developer and Graphic Designer from
+        Hey, I'm Joe DiGioia a Full Stack Developer and Graphic Designer from
         Pittsburgh, PA. <br />
-        I have a passion for creating new experiences through a mix of modern
-        design and web development.
+        I enjoy creating new user experiences through a mix of modern design and
+        web development.
       </p>
     </div>
   </section>
